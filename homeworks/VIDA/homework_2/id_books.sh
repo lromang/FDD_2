5e7df57f37a84c61b3d753a89a7bb7173e852f1c
Netohbk <fabiola@MacBook-Pro-de-Fabiola.local>
2022-08-25 16:53:23 -0500

    Corrección de un caracter

--- homeworks/VIDA/homework_2/id_books.sh
+++ homeworks/VIDA/homework_2/id_books.sh
@@ -4,7 +4,7 @@ echo 'Please write the id you want to download'
 
 read varnum
 
-until [[ $(cat pag_gu.txt | gsed -n "/^$varnum/p") == "$varnum" ]]
+until [[ $(cat pag_gu.txt | gsed -n "/^$varnum$/p") == "$varnum" ]]
 do
   echo 'Error. This id doesn´t exist in the file. Look the catalogue';
   cat pag_gu.txt
