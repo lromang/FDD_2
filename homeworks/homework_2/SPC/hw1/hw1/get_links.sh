5c60d7720a49d197f8f20cb233116184f109fee9
sammyboy86 <samuelpasta@gmail.com>
2022-09-20 20:11:57 -0500

    formato pedido

--- homeworks/homework_2/SPC/hw1/hw1/get_links.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-if [[ -f "links" ]]
-then
-    rm -r links
-fi
-
-for line in `grep -Eo '/ebooks/bookshelf/[0-9]|/ebooks/bookshelf/[0-9][0-9]|/ebooks/bookshelf/[0-9][0-9][0-9]' gutenberg_html.txt`;do echo https://gutenberg.org$line>>links; done
